f4b7655913e89c642233a1a5fa7074271c333e58
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-02-20 20:24:48 -0500

    added solution for multiply function

---
+++ challenges.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+# Challenge One: This code does not execute properly. Try to figure out why.
+# Challenge Source: https://www.codewars.com/kata/50654ddff44f800200000004/train/shell
+
+# Initial code
+# a=$1
+# b=$2
+# echo $((a*0))
+
+# Solution
+
+a=$1
+b=$2
+echo $((a*b))
