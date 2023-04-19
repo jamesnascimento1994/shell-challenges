749fe017c964c3745fb7ef2767bf9865322051ab
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-04-18 21:59:06 -0400

    added solution for sum of multiples of 3 or 5

--- challenges.sh
+++ challenges.sh
@@ -10,6 +10,27 @@
 
 # Solution
 
-a=$1
-b=$2
-echo $((a*b))
+# a=$1
+# b=$2
+# echo $((a*b))
+
+# Challenge Two: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+# Challenge Source: https://www.codewars.com/kata/514b92a657cdc65150000006/shell
+
+num=$1
+sum=0
+
+if (( num==0 ))
+then
+  echo 0
+else
+  for (( i=1 ; i<num ; ++i ))
+  do
+    if (( i%3==0 || i%5==0))
+    then
+      (( sum+=i ))
+    fi
+  done
+fi
+
+echo $sum
